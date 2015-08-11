b6e958597739a49a40dea0b44b48032a06cfebb9
StudioKris <kris@studiokris.com>
2015-08-11 23:03:39 +0200

    WIP

--- src/app/components/gallery/galleryItem.directive.js
+++ src/app/components/gallery/galleryItem.directive.js
@@ -18,15 +18,114 @@ class GalleryItemDirective {
 }
 
 class GalleryItemController {
-  constructor ($element, $scope) {
+  constructor ($element, $scope, $window, $document) {
     'ngInject';
 
+    this.window = $window;
+    this.document = $document;
+
     // "this.creation" is avaible by directive option "bindToController: true"
-    this.mainController = $scope.main;
+    this.mainController = $scope.$parent.$parent.main;
+    this.element = $element;
+    this.uiContainer = this.element.find('.item-container').get(0);
+    this.uiClip = this.element.find('.clip').get(0);
+    this.uiContent = this.element.find('.content').get(0);
+
+    this.mainController.addAnimatedItems(this);
   }
 
-  openItem() {
+  openItem($event) {
+    $event.stopPropagation();
+    this.mainController.openItem(this);
+  }
 
+  showItem() {
+    var slideContentDown = this._slideContentDown();
+    var clipImageIn = this._clipImageIn();
+    var floatContainer = this._floatContainer();
+    var clipImageOut = this._clipImageOut();
+    var slideContentUp = this._slideContentUp();
+
+    var sequence = new TimelineLite();
+
+    sequence.add(slideContentDown);
+    sequence.add(clipImageIn, 0);
+    sequence.add(floatContainer, '-=' + clipImageIn.duration() * 0.6);
+    sequence.add(clipImageOut, '-=' + floatContainer.duration() * 0.3);
+    sequence.add(slideContentUp, '-=' + clipImageOut.duration() * 0.6);
+
+    return sequence;
+  }
+
+  _slideContentDown() {
+    var tween = TweenLite.to(this.uiContent, 0.8, {
+      y: this.window.innerHeight,
+      ease: Expo.easeInOut
+    });
+
+    return tween;
+  }
+
+  _clipImageIn() {
+    var tween = TweenLite.to(this.uiClip, 0.8, {
+      attr: {
+        r: 60
+      },
+      ease: Expo.easeInOut
+    });
+
+    return tween;
+  }
+
+  _floatContainer() {
+    this.document.find('body').addClass('body-hidden');
+
+    var TL = new TimelineLite;
+
+    var rect = this.uiContainer.getBoundingClientRect();
+    var windowW = this.window.innerWidth;
+
+    TL.set(this.uiContainer, {
+      width: rect.width,
+      height: rect.height,
+      x: rect.left,
+      y: rect.top,
+      position: 'fixed',
+      overflow: 'hidden'
+    });
+
+    TL.to(this.uiContainer, 2, {
+      width: windowW,
+      height: '100%',
+      x: windowW / 2,
+      y: 0,
+      xPercent: -50,
+      ease: Expo.easeInOut,
+      clearProps: 'all',
+      className: '+=' + 'item-opened'
+    });
+
+    return TL;
+  }
+
+  _clipImageOut() {
+    var radius = this.uiClip.attributes['r'].value;
+
+    var tween = this._clipImageIn();
+
+    tween.vars.attr.r = radius;
+
+    return tween;
+  }
+
+  _slideContentUp() {
+    var tween = TweenLite.to(this.uiContent, 1, {
+      y: 0,
+      clearProps: 'all',
+      ease: Expo.easeInOut
+    });
+
+    return tween;
   }
 
   closeItem() {
@@ -34,7 +133,13 @@ class GalleryItemController {
   }
 
   hideItem() {
+    var tween = TweenLite.to(this.element, 0.4, {
+      y: this.window.innerHeight,
+      autoAlpha: 0,
+      ease: Expo.easeInOut
+    });
 
+    return tween;
   }
 }
 
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,11 +1,12 @@
 class MainController {
-	constructor($timeout, toastr, gallery, $window, $document) {
+	constructor($timeout, toastr, gallery, $window) {
 		'ngInject';
 
-		this.window = $window;
-		this.document = $document;
+    this.window = $window;
 
 		this.galleries = [];
+    this.animatedItems = [];
+
 		this.classAnimation = '';
 		this.creationDate = 1438525681651;
 		this.toastr = toastr;
@@ -26,17 +27,17 @@ class MainController {
 		}, 4000);
 	}
 
+  addAnimatedItems(itemController) {
+    this.animatedItems.push(itemController);
+  }
+
 	showToastr() {
 		this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
 		this.classAnimation = '';
 	}
 
-	openItem($event) {
-		$event.stopPropagation();
-		this.currentItem = angular.element($event.currentTarget).get(0);
-
-
-
+	openItem(item) {
+    this.currentItem = item;
 		var sequence = new TimelineLite({
 			paused: true
 		});
@@ -44,7 +45,7 @@ class MainController {
 		var otherItems = this._showHideOtherItems();
 
 		sequence.add(otherItems);
-		sequence.add(_openItem(), 0);
+		sequence.add(this.currentItem.showItem());
 
 		sequence.play();
 
@@ -52,126 +53,23 @@ class MainController {
 	}
 
 	_showHideOtherItems() {
-		var TL = new TimelineLite;
+		var sequence = new TimelineLite();
 
-		var items = this.document.find('.item-container');
+    var rect = this.currentItem.getBoundingClientRect();
+    var midX = rect.left + rect.width / 2;
+    var bottom = this.window.innerHeight;
 
-		for (var i in items) {
+		for (var i in this.animatedItems) {
+			var item = this.animatedItems[i];
 
-			var item = angular.element(items[i]);
+      
 
-			// When called with `openCard`.
 			if (this.currentItem !== item) {
-				TL.add(this._hideItem(item), 0);
+				sequence.add(item.hideItem(), '-=0.2');
 			}
 		}
 
-		return TL;
-	}
-
-	_hideItem(element) {
-
-		var tween = TweenLite.to(element, 0.4, {
-			scale: 0.8,
-			autoAlpha: 0,
-			transformOrigin: 'center bottom',
-			ease: Expo.easeInOut
-		});
-
-		return tween;
-	};
-
-	_openItem() {
-		var container = this.currentItem.find('.item-container').get(0);
-		var clip = this.currentItem.find('.clip').get(0);
-		var content = this.currentItem.find('.content').get(0);
-
-		var slideContentDown = this._slideContentDown(content);
-		var clipImageIn = this._clipImageIn(clip);
-		var floatContainer = this._floatContainer(container);
-		var clipImageOut = this._clipImageOut(clip);
-		var slideContentUp = this._slideContentUp(content);
-
-		var itemSequence = new TimelineLite();
-
-		itemSequence.add(slideContentDown);
-		itemSequence.add(clipImageIn, 0);
-		itemSequence.add(floatContainer, '-=' + clipImageIn.duration() * 0.6);
-		itemSequence.add(clipImageOut, '-=' + floatContainer.duration() * 0.3);
-		itemSequence.add(slideContentUp, '-=' + clipImageOut.duration() * 0.6);
-
-		return itemSequence;
-	}
-
-	_slideContentDown(element) {
-		var tween = TweenLite.to(element, 0.8, {
-			y: this.window.innerHeight,
-			ease: Expo.easeInOut
-		});
-
-		return tween;
-	}
-
-	_clipImageIn(element) {
-		var tween = TweenLite.to(element, 0.8, {
-			attr: {
-				r: 60
-			},
-			ease: Expo.easeInOut
-		});
-
-		return tween;
-	}
-
-	_floatContainer(element) {
-		this.document.find('body').addClass('body-hidden');
-
-		var TL = new TimelineLite;
-
-		var rect = element.getBoundingClientRect();
-		var windowW = this.window.innerWidth;
-
-		TL.set(element, {
-			width: rect.width,
-			height: rect.height,
-			x: rect.left,
-			y: rect.top,
-			position: 'fixed',
-			overflow: 'hidden'
-		});
-
-		TL.to(element, 2, {
-			width: windowW,
-			height: '100%',
-			x: windowW / 2,
-			y: 0,
-			xPercent: -50,
-			ease: Expo.easeInOut,
-			clearProps: 'all',
-			className: '-=' + 'item-container-closed'
-		});
-
-		return TL;
-	}
-
-	_clipImageOut(element) {
-		var radius = $(element).attr('r');
-
-		var tween = this._clipImageIn(element);
-
-		tween.vars.attr.r = radius;
-
-		return tween;
-	}
-
-	_slideContentUp(element) {
-		var tween = TweenLite.to(element, 1, {
-			y: 0,
-			clearProps: 'all',
-			ease: Expo.easeInOut
-		});
-
-		return tween;
+		return sequence;
 	}
 
 	closeItem($event) {
